b309f396a9cb0b85ce0c7d52479d3bbcb1b06bd6
Dean.Maunder <deanmaunder4@gmail.com>
2017-09-21 14:23:16 +1000

    Fix lambda code

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,16 +2,30 @@ import boto3
 import StringIO
 import zipfile
 
-s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('portfolio.maunder.link')
-build_bucket = s3.Bucket('portfolio-build.maunder.link')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('buildPortfolio.zip',portfolio_zip)
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    
+    topic = sns.Topic('arn:aws:sns:ap-southeast-2:198641624864:deployPortfolioTopic')
+    try:
+        portfolio_bucket = s3.Bucket('portfolio.maunder.link')
+        build_bucket = s3.Bucket('portfolio-build.maunder.link')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('buildPortfolio.zip',portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+        print "Job done!"
+    
+    
+        topic.publish(Subject="Portfolio Deployed", Message="Its done!")
+    except:
+        topic.publish(Subject="Portfolio FAILED", Message="Its NOT done!")
+        raise
